fb7bb2bc4e9eec0318d030c85d693ae2413a154e
Zahirul <jislam150@gmail.com>
2023-11-23 08:10:25 +0600

    Initial Commit

---
+++ StylePick/StylePickApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylePickApp.swift
+//  StylePick
+//
+//  Created by Hologram1 on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct StylePickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
